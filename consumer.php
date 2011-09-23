a1878129e3c17c51c277b2cabb2d949c67e3eb1b
Mikko Koppanen <mkoppanen@php.net>
2011-09-23 13:04:44 +0100

    Indent code with 4 spaces

--- consumer.php
+++ consumer.php
@@ -10,15 +10,12 @@ $s->connect("tcp://127.0.0.1:11133");
 $ack = new zmqsocket($z, zmq::SOCKET_PUSH); 
 $ack->connect("tcp://127.0.0.1:11132");
 
-//for ($i = 0; $i < 5; $i++) 
 
-$i = 0;
-while (true)
+for ($i = 0; $i < 10000; $i++) 
 {
     $response = $s->recvMulti();
     $ack->send ($response [0]);
     echo "Handled message $i" . PHP_EOL;
-    $i++;
 }
 
 	
--- producer.php
+++ producer.php
@@ -8,8 +8,9 @@ $s->connect("tcp://127.0.0.1:11131");
 
 sleep (1);
 
-while (true)
+for ($i = 0; $i < 100000000; $i++)
 {
-	$s->sendMulti (array ("msg", "aa", "bb", "cc"));
+    $s->sendMulti (array ("msg", "aa", "bb", "cc"));
     $s->recvmulti ();
+    echo "Sent message " . ($i + 1) . PHP_EOL;
 }	
